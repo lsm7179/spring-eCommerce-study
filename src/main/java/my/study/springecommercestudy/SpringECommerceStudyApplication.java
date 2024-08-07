fc4fff419cb3aecdccfb58d09e228184698debdc
이승민 <sml7179@naver.com>
2024-08-07 22:33:24 +0900

    Initial commit

---
+++ src/main/java/my/study/springecommercestudy/SpringECommerceStudyApplication.java
@@ -0,0 +1,13 @@
+package my.study.springecommercestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringECommerceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringECommerceStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/study/springecommercestudy/SpringECommerceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package my.study.springecommercestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringECommerceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
